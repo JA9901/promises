cae2df2560e7c5f8608c715f14c01201bbcb61f4
JA9901 <jareyes999@gmail.com>
2022-08-22 21:10:41 -0400

    finished promises tutorial

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status")
+const videoRef = document.querySelector(".video")
 
-// 1. Then
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email
-//   });
-
-// 2. Aync/Await  (the better and more professional way)
-async function main(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("FREE");
+        }, 2000);
+    })
 }
 
-main()
+function getVideo(subscriptionStatus){
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve("show video")
+        }
+        else if (subscriptionStatus === "FREE") {
+            resolve("show trailer")
+        }
+        else {
+            reject("no video")
+        }
+    })
+}
+
+
+// 2. Async/Await
+async function main() {
+    const status = (await getSubscriptionStatus())
+    statusRef.innerHTML = status
+    try {
+        console.log(await getVideo(status))
+    }
+    catch (e) {
+        console.log(e)
+        videoRef.innerHTML = e;
+    }
+}
+
+main();   
