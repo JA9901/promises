98c29253da2f515bf7549727609a237aa26614ba
JA9901 <jareyes999@gmail.com>
2022-08-22 20:46:57 -0400

    learning promises

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+const emailRef = document.querySelector(".email");
+
+// 1. Then
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// 2. Aync/Await  (the better and more professional way)
+async function main(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main()
